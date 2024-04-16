8f30d425494615f5683a3f7737a90fee15ec17d4
DUA-UMER <duam95623@gmail.com>
2024-04-17 01:43:39 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+// step 1:
+const answers = await inquirer.prompt([
+    { type: "number",
+        name: "numberOne",
+        message: "enter your first number"
+    },
+    { type: "number",
+        name: "numberTwo",
+        message: "enter your second number"
+    },
+    { type: "list",
+        name: "operator",
+        message: "enter your operator",
+        choices: ["+", "-", "*", "/"] }
+]);
+// step 2:
+const { numberOne, numberTwo, operator } = answers;
+let result;
+switch (operator) {
+    case "+":
+        result = numberOne + numberTwo;
+        break;
+    case "-":
+        result = numberOne - numberTwo;
+        break;
+    case "*":
+        result = numberOne * numberTwo;
+        break;
+    case "/":
+        result = numberOne / numberTwo;
+        break;
+    default:
+        console.log("Invalid operator");
+}
+console.log(chalk.yellow(`${numberOne} ${operator} ${numberTwo} = ${result}`));
